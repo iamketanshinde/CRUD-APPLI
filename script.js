c5191e1beb73524a4fb03be149079db7dc929553
Ketan Shinde <ketan_shinde@hotmail.com>
2024-06-23 22:44:17 +0530

    rendertask function is added

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ function addTask(){
    renderTask();
 }
 function renderTask(){
-    const TaskInput=document
+    const TaskInput=document.getElementById('taskInput');
+    TaskInput.innerHTML='';
+
+    tasks.forEach((task,index)=>{
+        const row=document.createElement('tr');
+        row.innerHTML=`
+        <td>${index+1}</td>
+        <td id="${task} onclick="tooglecomplete(${index})" style="cursor:pointer; ${task.completed?'text-decoration:line-through;':''}">${task.task}</td>
+        <td>
+        
+         <button class="btn btn-success mr-1 btn-sm" onclick="editTask(${index})">Edit</button>
+         <button class="btn btn-success mr-1 btn-sm" onclick="tooglecomplete(${completeTask})">${task.completed?'undo':'complete'}complete</button>
+         <button class="btn btn-success mr-1 btn-sm" onclick="deleteTask(${index})">delete</button>
+        </td>
+        `;
+        TaskInput.appendChild(row);
+    }).join();
 }
